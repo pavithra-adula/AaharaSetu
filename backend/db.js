1c646195e28984799ec9e810164aaf662f1052a6
pavithra-adula <anuadula@gmail.com>
2026-08-07 23:02:43 +0530

    Fix Aiven DB connection

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,19 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host:               process.env.DB_HOST,
-  user:               process.env.DB_USER,
-  password:           process.env.DB_PASSWORD,
-  database:           process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
   waitForConnections: true,
-  connectionLimit:    10,
-  queueLimit:         0
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const db = pool.promise();
